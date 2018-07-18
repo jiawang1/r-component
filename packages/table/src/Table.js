f8e43aae6042052472eb9ddc0c5741ea56c62771
jay.wang <jay.wang@ef.com>
2018-07-18 11:12:20 +0800

    merge table body and header to table file

--- packages/table/src/Table.js
+++ packages/table/src/Table.js
@@ -1,50 +1,135 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import TableHeader from './TableHeader';
-import TableBody from './TableBody';
-
-import { SELECT_ALL, SELECT_NONE } from './const';
+import CheckBox from './CheckBox';
 
 import './Table.css';
 
 class Table extends React.Component {
   constructor(props) {
     super(props);
-    this.test = '';
+    const { uniqueKey } = props;
+    this.uniqueKey = uniqueKey || 'id';
     this.state = {
-      selectAll: undefined
+      headerSelected: false,
+      selected: []
     };
-    this.selectAll = this.selectAll.bind(this);
-    this.resetSelectAll = this.resetSelectAll.bind(this);
+
+    this.onHeaderSelected = this.onHeaderSelected.bind(this);
   }
 
-  selectAll(val) {
-    const selectAll = val ? SELECT_ALL : SELECT_NONE;
-    this.setState({
-      selectAll
-    });
-  }
-  resetSelectAll(val) {
-    if (val !== this.state.selectAll) {
-      this.setState({ selectAll: val });
+  onHeaderSelected(headerSelected) {
+    const { data } = this.props;
+    const selected = [...this.state.selected];
+    this.setState({ headerSelected });
+    if (headerSelected) {
+      data.forEach(__data => {
+        if (this.state.selected.findIndex(sel => sel[this.uniqueKey] === __data[this.uniqueKey]) < 0) {
+          selected.push(__data);
+        }
+      });
+    } else {
+      data.forEach(__data => {
+        const inx = selected.findIndex(sel => sel[this.uniqueKey] === __data[this.uniqueKey]);
+        if (inx >= 0) {
+          selected.splice(inx, 1);
+        }
+      });
     }
+    this.setState({ headerSelected, selected });
+  }
+
+  selectionChange(oLine) {
+    return val => {
+      const selected = [...this.state.selected];
+      if (val) {
+        const { data } = this.props;
+        const state = { selected };
+        selected.push(oLine);
+
+        if (
+          data.every(__data => selected.findIndex(sel => sel[this.uniqueKey] === __data[this.uniqueKey]) >= 0)
+        ) {
+          state.headerSelected = true;
+        }
+        this.setState(state);
+      } else {
+        selected.splice(
+          this.state.selected.findIndex(sel => sel[this.uniqueKey] === oLine[this.uniqueKey]),
+          1
+        );
+        this.setState({
+          selected,
+          headerSelected: false
+        });
+      }
+    };
+  }
+
+  renderHeader() {
+    const { columns, selection } = this.props;
+    const __columns = columns.slice();
+    if (selection) {
+      __columns.unshift({
+        render: () => <CheckBox value={this.state.headerSelected} onChange={this.onHeaderSelected} />
+      });
+    }
+    return (
+      <div className="p-header-frame">
+        <table className="p-header">
+          <thead>
+            <tr>
+              {__columns.map((col, inx) => (
+                <th className={`${inx === 0 ? 'p-selection-header' : 'p-header-col'}`} key={inx}>
+                  {col.render ? col.render() : col.title}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody />
+        </table>
+      </div>
+    );
+  }
+
+  renderLine(data) {
+    const { selection } = this.props;
+    const results = [];
+    if (selection) {
+      results.push(
+        <td key={-1} className="p-selection-col">
+          <CheckBox
+            onChange={this.selectionChange(data)}
+            value={this.state.selected.some(sel => sel[this.uniqueKey] === data[this.uniqueKey])}
+          />
+        </td>
+      );
+    }
+    return results.concat(
+      Object.keys(data).map(k => (
+        <td key={k} className="p-body-col">
+          {data[k]}
+        </td>
+      ))
+    );
+  }
+
+  renderBody() {
+    const { data } = this.props;
+    return (
+      <div className="p-body-frame">
+        <table className="p-body">
+          <thead />
+          <tbody>{data.map(__data => <tr>{this.renderLine(__data)}</tr>)}</tbody>
+        </table>
+      </div>
+    );
   }
 
   render() {
-    const { columns, ...bodyProps } = this.props;
-    const { data, ...headerProps } = this.props;
-
-    if (this.props.selection) {
-      bodyProps.selectAll = this.state.selectAll;
-      bodyProps.resetSelectAll = this.resetSelectAll;
-      headerProps.selectAllCb = this.selectAll;
-      headerProps.selectAll = this.state.selectAll;
-    }
     return (
       <div>
-        <TableHeader {...headerProps} />
-        <TableBody {...bodyProps} />
+        {this.renderHeader()}
+        {this.renderBody()}
       </div>
     );
   }
